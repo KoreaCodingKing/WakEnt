93a8509352106cd5f53ed03e5a12995fa5872b5b
yrkim <yrkim@pocl.io>
2021-12-03 15:25:54 +0900

    문구 추가

--- src/components/main/Home.tsx
+++ src/components/main/Home.tsx
@@ -12,12 +12,15 @@ const Home = () => {
                 </div>
                 <div className="subtitle_container">
                     <p>다양한 컨텐츠 세상</p>
+                    <p>컨텐츠를 만들어가고 다양한 컨텐츠를 창조합니다.</p>
                 </div>
                 <div className="subtitle_container">
                     <p>레게노한 미래 지향 컨텐츠들</p>
+                    <p>메타버스의 시대를 맞춘 새로운 컨텐츠를 선사드립니다.</p>
                 </div>
                 <div className="subtitle_container">
                     <p>알작딱한 개인 방송 컨텐츠들</p>
+                    <p>남녀노소 누구나 즐길 수 있는 창조적인 컨텐츠를 만들어갑니다.</p>
                 </div>
                 <div className="subtitle_image_container">
                     
