3860bd3b4fdea51ecdb5c560ff4c1bb81075d2fa
yrkim <yrkim@pocl.io>
2021-12-22 19:11:15 +0900

    Header 수정, 파일 경로 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,16 @@
-import { useEffect, useRef } from 'react';
-
 import { NextPage } from 'next';
+import Header from '../components/Header';
 // import Head from 'next/head'
 // import Image from 'next/image'
 
 const Home: NextPage = () => {
-  return <div className='main'></div>;
+  return <div className='main'>
+    <header>
+      <Header>
+        {/* ToDo: fix home header contents */}
+      </Header>
+    </header>
+  </div>;
 };
 
 export default Home;
