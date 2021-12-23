03b82370e7b4f2c0b05485e36006e4bd0adb5acb
So-chiru <unleash9224@gmail.com>
2021-12-23 19:52:46 +0900

    Refactor main.tsx

--- pages/isedol/main.tsx
+++ pages/isedol/main.tsx
@@ -1,27 +1,30 @@
+import { NextPage } from 'next';
 import { useEffect, useRef, useState } from 'react';
 
 import styles from '../../styles/components/isedol/isedolMain.module.scss';
 
-export const Main = () => {
+export const Main: NextPage = () => {
   // ToDo: 클릭때마다 setCurrentIndex로 currentIndex 변경
   const [currentIndex, setCurrentIndex] = useState(0);
   const containerRef = useRef<HTMLDivElement>(null);
-  const backgroundImagesPath = [
-    { index: 0, path: '/images/bg_isedol_rewind.png'}
+  const slides = [
+    {
+      image: '/images/bg_rewind.jpg',
+      title: 'RE : WIND',
+      subtitle: '2021.12.22 MV Released',
+    },
   ];
 
-  useEffect(() => {
-    const currentImagePath = backgroundImagesPath[currentIndex].path;
-    containerRef.current!.style.backgroundImage = `url(${currentImagePath})`;
-  }, [currentIndex]);
-
   return (
-    <div className={styles.isedol_main__container} ref={containerRef}>
+    <div
+      className={styles.isedol_main__container}
+      style={{ backgroundImage: `url(${slides[currentIndex]})` }}
+    >
       <h2 className={styles.isedol_main__title}>
         RE<span>:</span>WIND
       </h2>
       <div className={styles.isedol_main__subtitle_box}>
-        <p className={styles.subtitle}>2021.12.22 MV Released</p>
+        <p className={styles.subtitle}></p>
         <button className={styles.play_btn}></button>
       </div>
       <div className={styles.isedol_main__slide_box}>
