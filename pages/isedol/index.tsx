42e3d354136b6edf307b233a6823fd940a573884
yrkim <yrkim@pocl.io>
2021-12-23 12:50:03 +0900

    콘텐츠 영역, header 영역 나누기

--- pages/isedol/index.tsx
+++ pages/isedol/index.tsx
@@ -1,13 +1,20 @@
 import { NextPage } from 'next';
 import { useState } from 'react';
+
+import styles from '../../styles/components/isedol/isedoleMain.module.scss';
 import IsedolHeader from '../../components/isedol/IsedolHeader';
 
 const IsedolPage: NextPage = (): JSX.Element => {
   const [isOpenMenu, setIsOpenMenu] = useState(false);
 
-  return <div className='main'>
-    <IsedolHeader isOpenMenu={isOpenMenu}></IsedolHeader>
-  </div>;
+  return (
+    <div className={styles.isedol_container}>
+      <IsedolHeader isOpenMenu={isOpenMenu}></IsedolHeader>
+      <div className={styles.isedol_contents}>
+        {/* ToDo: 메인, 멤버, discography등 보여주는 콘텐츠 */}
+      </div>
+    </div>
+  );
 };
 
 export default IsedolPage;
