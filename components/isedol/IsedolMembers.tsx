2de3515514831f2d6bc7db869d8db83f35ab92a7
yrkim <yrkim@pocl.io>
2021-12-24 10:47:09 +0900

    state 상수 추가, members 상수 추가, 불필요 코드 제거

--- components/isedol/IsedolMembers.tsx
+++ components/isedol/IsedolMembers.tsx
@@ -1,9 +1,19 @@
 import { NextPage } from 'next';
+import { useState } from 'react';
 import styles from '../../styles/components/isedol/IsedolMembers.module.scss';
 
 export const IsedolMembers: NextPage = () => {
+  const [selectMember, setSelectMember] = useState(0);
+  const members = [
+    {name: 'ine'},
+    {name: 'jingburger'},
+    {name: 'lilpa'},
+    {name: 'jururu'},
+    {name: 'gosegu'},
+    {name: 'vichan'},
+  ]
   return (
-    <div className={styles.isedol_main__container}>
+    <div className={styles.isedol_member__container}>
       <div className={styles.background}></div>
     </div>
   );
