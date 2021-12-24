378b98333f64996c41642ba52ace67d921b60ad0
yrkim <yrkim@pocl.io>
2021-12-24 10:57:57 +0900

    image path 추가

--- components/isedol/IsedolMembers.tsx
+++ components/isedol/IsedolMembers.tsx
@@ -5,12 +5,12 @@ import styles from '../../styles/components/isedol/IsedolMembers.module.scss';
 export const IsedolMembers: NextPage = () => {
   const [selectMember, setSelectMember] = useState(0);
   const members = [
-    {name: 'ine'},
-    {name: 'jingburger'},
-    {name: 'lilpa'},
-    {name: 'jururu'},
-    {name: 'gosegu'},
-    {name: 'vichan'},
+    {name: 'ine', image: '/images/bg_ine.png'},
+    {name: 'jingburger', image: '/images/bg_jingburger.png'},
+    {name: 'lilpa', image: '/images/bg_lilpa.png'},
+    {name: 'jururu', image: '/images/bg_jururu.png'},
+    {name: 'gosegu', image: '/images/bg_gosegu.png'},
+    {name: 'vichan', image: '/images/bg_vichan.png'},
   ]
   return (
     <div className={styles.isedol_member__container}>
