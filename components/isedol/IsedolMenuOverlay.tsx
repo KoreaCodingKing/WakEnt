7ef29a2e771d920b6898045ed6920f3726324fe1
So-chiru <unleash9224@gmail.com>
2021-12-24 11:44:47 +0900

    Complete Menu Overlay

--- components/isedol/IsedolMenuOverlay.tsx
+++ components/isedol/IsedolMenuOverlay.tsx
@@ -1,13 +1,58 @@
+import Link from 'next/link';
 import styles from '../../styles/components/isedol/IsedolMenuOverlay.module.scss';
 import { concatClass } from '../../utils/class';
+import LinkToIcon from '../common/icons/LinkTo';
+import { WakEnterLogo } from '../wakenter/WakEnterHeader';
 
 interface IsedolMenuOverlayProps {
   open: boolean
 }
 
+const Links = [
+  {
+    name: 'MEMBER',
+    page: '/isedol/member',
+  },
+  {
+    name: 'DISCOGRAPHY',
+    page: '/isedol/discography'
+  },
+  {
+    name: 'YouTube',
+    page: 'https://youtube.com/c/welshcorgimessi'
+  },
+  {
+    name: 'WAKZOO',
+    page: 'https://cafe.naver.com/steamindiegame'
+  }
+];
+
 export const IsedolMenuOverlay = ({ open }: IsedolMenuOverlayProps) => {
   return (
     <div className={concatClass(styles.overlay, open && styles.open)}>
+      <div className={styles.contents}>
+        <div className={styles.links}>
+          {
+            Links.map(v => <Link href={v.page} passHref>
+              <div className={styles.link}>
+                <h1>{v.name}</h1>
+                {
+                  v.page[0] !== '/' && <LinkToIcon></LinkToIcon>
+                }
+              </div>
+            </Link>)
+          }
+        </div>
+        <div className={styles.attribution}>
+          <p>Background Illustration by SE2RA on WAKZOO.</p>
+          <p>Profile Photo by 여비날 on WAKZOO.</p>
+        </div>
+        <Link href={'/'} passHref>
+          <div className={styles.logo}>
+            <WakEnterLogo></WakEnterLogo>
+          </div>
+        </Link>
+      </div>
     </div>
   );
 };
