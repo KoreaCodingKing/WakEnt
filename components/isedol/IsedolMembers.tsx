07ead96fe328aca512743d98b76283a22aa5827b
yrkim <yrkim@pocl.io>
2021-12-25 19:58:37 +0900

    고세구 위치 수정

--- components/isedol/IsedolMembers.tsx
+++ components/isedol/IsedolMembers.tsx
@@ -117,7 +117,7 @@ export const IsedolMembers: NextPage = () => {
                 {true &&
                   <div
                     className={styles.sign_box}
-                    data-member={'44'}>
+                    data-member={'55'}>
                     <div className={styles.arrow_wrapper}>
                       <Image
                         className={styles.sign_arrow}
