00c20fe9cfd452ab156ed674b5e4a72c59fc249c
So-chiru <unleash9224@gmail.com>
2021-12-25 14:00:23 +0900

    Add blurhash image loader

--- components/isedol/AlbumCard.tsx
+++ components/isedol/AlbumCard.tsx
@@ -1,16 +1,18 @@
 import Image from 'next/image';
 import styles from '../../styles/components/isedol/CardBase.module.scss';
+import ImageLoader from '../common/ImageLoader';
 
 interface AlbumCardProps {
   title: string
   image: string
   link: string
+  blurhash?: string
 }
 
-export const AlbumCard = ({ title, link, image }: AlbumCardProps) => {
+export const AlbumCard = ({ title, link, image, blurhash }: AlbumCardProps) => {
   return <div className={styles.card}>
     <div className={styles.image_wrapper}>
-      <Image src={image} layout='fill'></Image>
+      <ImageLoader blurDataURL={blurhash} src={image} layout='fill'></ImageLoader>
     </div>
     <h3 className={styles.title}>{ title }</h3>
   </div>;
--- components/isedol/YouTubeCard.tsx
+++ components/isedol/YouTubeCard.tsx
@@ -1,24 +1,23 @@
 import Image from 'next/image';
 import styles from '../../styles/components/isedol/CardBase.module.scss';
 import { concatClass } from '../../utils/class';
+import ImageLoader from '../common/ImageLoader';
 
 interface YouTubeCardProps {
   title: string
+  blurhash?: string
   id: string
   onClick?: (id: string) => void
 }
 
-export const YouTubeCard = ({ title, id, onClick }: YouTubeCardProps) => {
+export const YouTubeCard = ({ title, id, onClick, blurhash }: YouTubeCardProps) => {
   return (
     <div
       className={concatClass(styles.card, onClick && styles.clickable)}
       onClick={() => onClick && onClick(id)}
     >
       <div className={styles.youtube_wrapper}>
-        <Image
-          src={`https://img.youtube.com/vi/${id}/maxresdefault.jpg`}
-          layout='fill'
-        ></Image>
+        <ImageLoader blurDataURL={blurhash} src={`https://img.youtube.com/vi/${id}/maxresdefault.jpg`} layout='fill'></ImageLoader>
       </div>
       <h3 className={styles.title}>{title}</h3>
     </div>
