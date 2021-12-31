9810659e5e0a3b827f01d3058f84b2dd3eea1327
So-chiru <unleash9224@gmail.com>
2021-12-31 16:30:43 +0900

    Add header links

--- components/wakenter/WakEnterHeader.tsx
+++ components/wakenter/WakEnterHeader.tsx
@@ -4,16 +4,40 @@ import Image from 'next/image';
 
 import styles from '../../styles/components/wakenter/WakEnterHeader.module.scss';
 import { concatClass } from '../../utils/class';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
-interface WakEnterLogoProps {
+interface WakEnterElementProps {
+  clickable?: boolean
   white?: boolean
 }
 
-export const WakEnterLogo = ({ white }: WakEnterLogoProps) => {
+const Menus = [
+  {
+    title: 'About',
+    link: '/about',
+  },
+  {
+    title: '채용',
+    link: '/recruit',
+  },
+];
+
+export const WakEnterLogo = ({ white, clickable }: WakEnterElementProps) => {
   return (
-    <div className={concatClass(styles.logo, white && styles.white)}>
+    <div
+      className={concatClass(
+        styles.logo,
+        white && styles.white,
+        clickable && styles.clickable
+      )}
+    >
       <div className={styles.logoImage}>
-        <Image src='/images/wak-enter-centered.png' layout='fill' alt='WAK'></Image>
+        <Image
+          src='/images/wak-enter-centered.png'
+          layout='fill'
+          alt='WAK'
+        ></Image>
       </div>
       <div className={styles.logoText}>
         <span>ENTERTAINMENT</span>
@@ -22,17 +46,34 @@ export const WakEnterLogo = ({ white }: WakEnterLogoProps) => {
   );
 };
 
-export const WakEnterHeader = (props: HeaderBaseProps) => {
-  const Left = <WakEnterLogo white={props.white}></WakEnterLogo>;
-  const Right = <></>; // TODO : 오른쪽 부분 추가하기
-
+const WakMenus = ({ white }: WakEnterElementProps) => {
   return (
-    <HeaderBase
-      {...props}
-      left={Left}
-      right={Right}
-    ></HeaderBase>
+    <div className={concatClass(styles.menu, white && styles.white)}>
+      {Menus.map(menu => (
+        <Link key={`link-${menu.title}`} href={menu.link}>
+          <a className={styles.link}>{menu.title}</a>
+        </Link>
+      ))}
+    </div>
   );
+};
+
+export const WakEnterHeader = (props: HeaderBaseProps) => {
+  const router = useRouter();
+
+  const Left = (
+    <Link href='/' passHref>
+      <span
+        tabIndex={100}
+        onKeyDown={ev => ev.key === 'Enter' && router.push('/')}
+      >
+        <WakEnterLogo white={props.white} clickable></WakEnterLogo>
+      </span>
+    </Link>
+  );
+  const Right = <WakMenus white={props.white}></WakMenus>;
+
+  return <HeaderBase {...props} left={Left} right={Right} full></HeaderBase>;
 };
 
 export default WakEnterHeader;
