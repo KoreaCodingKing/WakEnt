209ef1100883fffeda631fd951a25358f44ba958
yrkim <yrkim@pocl.io>
2022-01-09 19:22:20 +0900

    코드 리팩토링

--- components/wakenter/AboutPageSections/SecondSection.tsx
+++ components/wakenter/AboutPageSections/SecondSection.tsx
@@ -11,71 +11,53 @@ interface SecondSectionProps {
   onScroll: (index: number, callback: scrollHandler) => void
 }
 
-interface OfficeImages {
+interface OfficeImage {
   path: string,
-  desc: string
-}
-
-interface ImageLocation {
+  desc: string,
   left: MotionValue<number>,
   top: MotionValue<number>
 }
 
-const officeImages: OfficeImages[] = [
-  {
-    path: '/images/building/officetemp/bg_office_enterance.png',
-    desc: '왁엔터테인먼트 입구'
-  },
-  {
-    path: '/images/building/officetemp/bg_office_enterance_door.png',
-    desc: '왁엔터테인먼트 입구 현관'
-  },
-  {
-    path: '/images/building/officetemp/bg_office_info.png',
-    desc: '왁엔터테인먼트 안내데스크'
-  },
-  {
-    path: '/images/building/officetemp/bg_office_hallway.png',
-    desc: '왁엔터테인먼트 사무실 복도'
-  },
-  {
-    path: '/images/building/officetemp/bg_office_lounge.png',
-    desc: '왁엔터테인먼트 라운지'
-  },
-  {
-    path: '/images/building/officetemp/bg_office_audio_visual_room.png',
-    desc: '왁엔터테인먼트 시청각실'
-  }
-];
-
 const SecondSection = ({className, onScroll}: SecondSectionProps) => {
   // ToDo: 이미지 기본 위치 지정 필요
-  const imagesLocation = [
+  const officeImages: OfficeImage[] = [
     {
+      path: '/images/building/officetemp/bg_office_enterance.png',
+      desc: '왁엔터테인먼트 입구',
       left: useMotionValue(15),
       top: useMotionValue(0)
     },
     {
+      path: '/images/building/officetemp/bg_office_enterance_door.png',
+      desc: '왁엔터테인먼트 입구 현관',
       left: useMotionValue(45),
       top: useMotionValue(0)
     },
     {
+      path: '/images/building/officetemp/bg_office_info.png',
+      desc: '왁엔터테인먼트 안내데스크',
       left: useMotionValue(75),
       top: useMotionValue(0)
     },
     {
+      path: '/images/building/officetemp/bg_office_hallway.png',
+      desc: '왁엔터테인먼트 사무실 복도',
       left: useMotionValue(15),
       top: useMotionValue(20)
     },
     {
+      path: '/images/building/officetemp/bg_office_lounge.png',
+      desc: '왁엔터테인먼트 라운지',
       left: useMotionValue(45),
       top: useMotionValue(20)
     },
     {
+      path: '/images/building/officetemp/bg_office_audio_visual_room.png',
+      desc: '왁엔터테인먼트 시청각실',
       left: useMotionValue(75),
       top: useMotionValue(20)
-    },
-  ] as ImageLocation[];
+    }
+  ];
 
   useEffect(() => {
     onScroll(1, (top, height) => {
@@ -84,7 +66,7 @@ const SecondSection = ({className, onScroll}: SecondSectionProps) => {
       console.log('height', height);
 
       if ((height / top) * 100 > 80) {
-        imagesLocation.forEach((imageLocation: ImageLocation, index: number) => {
+        officeImages.forEach((officeImage: OfficeImage, index: number) => {
           // 스크롤 이후 이미지 위치 이동.
           // transition 속성 추가.
         });
@@ -97,15 +79,15 @@ const SecondSection = ({className, onScroll}: SecondSectionProps) => {
   return(
     <section className={concatClass(className)} data-index={1}>
       <div className={styles.second_section_inner}>
-        {officeImages.map((officeImage: OfficeImages, index: number) => {
+        {officeImages.map((officeImage: OfficeImage, index: number) => {
           return (
             <motion.div
               key={index}
               className={styles.image_container}
               style={{
                 zIndex: index,
-                left: imagesLocation[index].left,
-                top: imagesLocation[index].top 
+                left: officeImages[index].left,
+                top: officeImages[index].top 
               }}>
               <div className={styles.image_inner_container}>
                 <Image
