f609b09d2607dc03630e43b01e89547bd82151c3
yrkim <yrkim@pocl.io>
2022-01-13 21:58:39 +0900

    중복 scss 코드 mixin, 이미지 추가

--- components/wakenter/AboutPageSections/FourthSection.tsx
+++ components/wakenter/AboutPageSections/FourthSection.tsx
@@ -1,5 +1,6 @@
 import { scrollHandler } from "../../../pages/about";
 import { concatClass } from "../../../utils/class";
+import styles from '../../../styles/components/wakenter/AboutPageSections/FourthSection.module.scss';
 
 interface FourthSectionProps {
   className: string,
@@ -10,7 +11,12 @@ interface FourthSectionProps {
 const FourthSection = ({ className, onScroll }: FourthSectionProps) => {
   return (
     <section className={concatClass(className)} data-index={3}>
-
+      <div className={styles.section_container}>
+        <div className={styles.contents_wrapper}>
+          <div className={styles.infra_introduce}></div>
+          <div className={styles.crew_container}></div>
+        </div>
+      </div>
     </section>
   )
 }
