a03dd108bce3c46fc208740fb3346da72061dc2b
So-chiru <unleash9224@gmail.com>
2022-01-16 12:27:13 +0900

    Add other planet

--- components/gomem/Gomem3D.tsx
+++ components/gomem/Gomem3D.tsx
@@ -1,26 +1,82 @@
 import * as THREE from 'three';
 import React, { Suspense, useEffect, useRef } from 'react';
-import { Canvas, useFrame, useLoader } from '@react-three/fiber';
+import { Canvas, useFrame, useLoader, useThree } from '@react-three/fiber';
 import { Bloom, EffectComposer } from '@react-three/postprocessing';
 
 import WakDoo from '../../public/images/wakdoo.png';
+import SpaceImage from '../../public/images/space.jpg';
+import EarthImage from '../../public/images/earth-day.jpg';
+import IsedolImage from '../../public/images/logo_isedol.png';
+
 import { MotionValue, useSpring } from 'framer-motion';
 import { OrbitControls } from '@react-three/drei';
 
-const Globe = (props: JSX.IntrinsicElements['mesh']) => {
+import { motion } from "framer-motion/three";
+
+const IsedolGlobe = (props: JSX.IntrinsicElements['mesh']) => {
   const mesh = useRef<THREE.Mesh>(null!);
-  const texture = useLoader(THREE.TextureLoader, WakDoo.src);
+  const texture = useLoader(THREE.TextureLoader, IsedolImage.src);
 
   useFrame((state, delta) => {
     mesh.current.rotation.y += 0.003;
   });
 
   return (
-    <mesh {...props} ref={mesh}>
+    <motion.mesh {...props} ref={mesh} castShadow whileHover={{ scale: 1.1 }}>
       <sphereGeometry args={[2, 32, 32]} />
-      <meshPhongMaterial attach='material' map={texture} />
-    </mesh>
+      <meshToonMaterial attach='material' map={texture} />
+    </motion.mesh>
   );
+};
+
+const EarthGlobe = (props: JSX.IntrinsicElements['mesh']) => {
+  const mesh = useRef<THREE.Mesh>(null!);
+  const texture = useLoader(THREE.TextureLoader, EarthImage.src);
+
+  useFrame((state, delta) => {
+    mesh.current.rotation.y += 0.003;
+  });
+
+  return (
+    <motion.mesh {...props} ref={mesh} castShadow whileHover={{ scale: 1.1 }}>
+      <sphereGeometry args={[2, 32, 32]} />
+      <meshToonMaterial attach='material' map={texture} />
+    </motion.mesh>
+  );
+};
+
+const Sun = (props: JSX.IntrinsicElements['mesh']) => {
+  const mesh = useRef<THREE.Mesh>(null!);
+
+  const texture = useLoader(THREE.TextureLoader, WakDoo.src);
+
+  useFrame((state, delta) => {
+    mesh.current.rotation.y += 0.0005;
+  });
+
+  return (
+    <motion.mesh {...props} ref={mesh} whileHover={{ scale: 1.6 }}>
+      <sphereGeometry args={[16, 32, 32]} />
+      <meshToonMaterial attach='material' map={texture} />
+    </motion.mesh>
+  );
+};
+
+const Skybox = () => {
+  const { scene } = useThree();
+  const loader = new THREE.CubeTextureLoader();
+
+  const texture = loader.load([
+    WakDoo.src,
+    SpaceImage.src,
+    SpaceImage.src,
+    SpaceImage.src,
+    SpaceImage.src,
+    SpaceImage.src,
+  ]);
+
+  scene.background = texture;
+  return null;
 };
 
 interface CameraProps {
@@ -56,13 +112,16 @@ export const Gomem3D = ({
       <Camera progress={progress}></Camera>
       <OrbitControls enableZoom={false} enablePan={false}></OrbitControls>
       <ambientLight intensity={0.05} />
-      <pointLight position={[3, 3, 3]} />
+      <pointLight position={[0, 0, 100]} />
+      <Skybox></Skybox>
       <Suspense fallback={null}>
-        <Globe position={[0, 0, 0]} />
+        <EarthGlobe position={[0, 0, 0]} />
+        <IsedolGlobe position={[0, 0, -20]} />
+        <Sun position={[0, 0, 100]}></Sun>
       </Suspense>
       <EffectComposer>
         {/* <DepthOfField focusDistance={0} focalLength={0.02} bokehScale={2} height={480} /> */}
-        <Bloom luminanceThreshold={0} luminanceSmoothing={0} height={300} />
+        <Bloom luminanceThreshold={0} luminanceSmoothing={0} intensity={0.2} height={300} />
       </EffectComposer>
     </Canvas>
   );
