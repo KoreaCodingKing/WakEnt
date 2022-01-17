96d50d8378715bc85c3cc35de615599f6f945167
So-chiru <unleash9224@gmail.com>
2022-01-17 17:14:30 +0900

    Click to move page

--- pages/gomem/index.tsx
+++ pages/gomem/index.tsx
@@ -1,12 +1,17 @@
 import { NextPage } from 'next';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import Gomem3D from '../../components/gomem/Gomem3D';
 import WakEnterMetadata from '../../components/wakenter/Meta';
+import { WakEnterLogo } from '../../components/wakenter/WakEnterHeader';
 
 import styles from '../../styles/pages/gomem/index.module.scss';
 
 const Gomem: NextPage = () => {
   const [sceneActive, setSceneActive] = useState<boolean>(false);
+
+  const router = useRouter();
 
   useEffect(() => {
     requestAnimationFrame(() => {
@@ -18,13 +23,19 @@ const Gomem: NextPage = () => {
     <>
       <WakEnterMetadata title='고정 멤버'></WakEnterMetadata>
       <div className={styles.main}>
-        <header>
-          {/* <WakEnterHeader white={page === 1}></WakEnterHeader> */}
-        </header>
         <div className={styles.gomem3D}>
           <Gomem3D sceneActive={sceneActive}></Gomem3D>
         </div>
       </div>
+      <Link key={'link-wak-enter'} href={'/'} passHref>
+        <div
+          className={styles.logo}
+          tabIndex={100}
+          onKeyDown={ev => ev.key === 'Enter' && router.push('/')}
+        >
+          <WakEnterLogo white></WakEnterLogo>
+        </div>
+      </Link>
     </>
   );
 };
