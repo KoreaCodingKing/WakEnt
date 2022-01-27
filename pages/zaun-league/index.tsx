c3f07f173fa3c53bec654f4770f3f79bbd515dc6
yrkim <yrkim@pocl.io>
2022-01-27 23:23:09 +0900

    링크 메타 썸네일 이미지 변경

--- pages/zaun-league/index.tsx
+++ pages/zaun-league/index.tsx
@@ -12,7 +12,7 @@ const ZaunLeague: NextPage = (): JSX.Element => {
         title='ZAUN-LEAGUE'
         scope='LEAGUE OF ZAUN'
         description='WAK Entertainment 공식 주관 프로/아마추어 롤 리그'
-        image='/images/zaun/logo_zaun_black.png'
+        image='/images/zaun/thumbnail_zaun.png'
       ></WakEnterMetadata>
       <div className={styles.zaun_header}>
         <ZaunHeader></ZaunHeader>
