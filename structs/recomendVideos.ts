b7c25d6913711684b1ccf6bcc35e8c6756f19466
yrkim <yrkim@pocl.io>
2022-02-05 03:51:05 +0900

    indet fix

--- structs/recomendVideos.ts
+++ structs/recomendVideos.ts
@@ -1,40 +1,40 @@
 interface YouTubeCard {
-    title: string
-    id: string
-    thumbnail?: string
-  }
-  
+  title: string
+  id: string
+  thumbnail?: string
+}
+
 export const Cards: YouTubeCard[] = [
-{
+  {
     title: '왕이 된 고세구',
     id: 'Uj0veiazH_A',
-},
-{
+  },
+  {
     title: 'It\'s Beginning To Look A Lot Like Christmas - ISEGYE IDOL',
     id: 'kNPykP_9wOQ',
-},
-{
+  },
+  {
     title: '세계 최초 메타버스 강의 - VR챗 학교',
     id: 'G531ABhyxfE'
-},
-{
+  },
+  {
     title: 'Wak Miserables (우왁굳 반응)',
     id: 'ys52M8cwuNk'
-},
-{
+  },
+  {
     title: 'My WAKTAVERSE',
     id: 'Kc85nOaqLwo'
-},
-{
+  },
+  {
     title: '이세돌 x 고멤 골프 합방',
     id: 'ineOjHIBQGk'
-},
-{
+  },
+  {
     title: '수정구슬이 내는 힌트를 보고 그림을 맞추는 게임',
     id: '6ZKSWy3uNes'
-},
-{
+  },
+  {
     title: '마리오파티 왁타버스 올스타 멤버들로 합방!',
     id: 'sfC8urnJx6Y'
-}
+  }
 ];
