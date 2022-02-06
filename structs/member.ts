4936f59740d6fc9280dbe536f9f0c639518631cb
So-chiru <unleash9224@gmail.com>
2022-02-06 16:12:46 +0900

    Update member description

--- structs/member.ts
+++ structs/member.ts
@@ -53,7 +53,7 @@ export type GomemMemberID =
   | 'Wakpago'
   | 'BusinessKim'
   | 'GwakChoonSik'
-  | 'GwonMin'
+  | 'KwonMin'
   | 'DrDopamine'
   | 'FriedShrimp'
   | 'MitsuneHaku'
@@ -91,7 +91,7 @@ export const GomemUnits: Record<GomemUnitID, GomemUnitMetadata> = {
   [GomemUnit.Specter]: {
     name: '망령촌',
     description: '고멤이었던 자들의 무덤. 이들은 고멤에 한이 맺혀 망령으로 남아 있다.',
-    members: ['Dandap', 'CallyCarly', 'Wakpago', 'BusinessKim', 'GwakChoonSik', 'GwonMin'],
+    members: ['Dandap', 'CallyCarly', 'Wakpago', 'BusinessKim', 'GwakChoonSik', 'KwonMin'],
   },
   [GomemUnit.Gomem]: {
     name: '고멤 시즌 2',
@@ -108,7 +108,7 @@ export const GomemUnits: Record<GomemUnitID, GomemUnitMetadata> = {
 export const GomemSeason2Members: Record<GomemMemberID, GomemMember> = {
   Dandap: {
     name: {
-      en: 'Dandap',
+      en: 'Dandap Bug',
       ko: '단답벌레',
     },
     description: '단답.',
@@ -118,9 +118,9 @@ export const GomemSeason2Members: Record<GomemMemberID, GomemMember> = {
   CallyCarly: {
     name: {
       en: 'Cally Carly Davidson',
-      ko: '캘리칼리 데이비슨',
+      ko: '캘리컬리 데이비슨',
     },
-    description: '캘리칼리.',
+    description: '나이 37세. 상남자. 독보적.',
     image: '/images/model/gomem/callycarly/body.png',
     links: [],
   },
@@ -129,7 +129,7 @@ export const GomemSeason2Members: Record<GomemMemberID, GomemMember> = {
       en: 'Wakpago',
       ko: '왁파고',
     },
-    description: '왁파고.',
+    description: '원래 자신은 우왁굳이 만든 것이라고 주장했지만? 과학 패밀리의 도파민이 만든 것으로 밝혀졌다.',
     image: '/images/model/gomem/wakpago/body.png',
     links: [],
   },
@@ -138,34 +138,34 @@ export const GomemSeason2Members: Record<GomemMemberID, GomemMember> = {
       en: 'Business Kim',
       ko: '비즈니스 킴',
     },
-    description: '비킴.',
+    description: '미국 버즈니아 출신, 가문으로는 신성 로마제국의 귀족 출신이다.',
     image: '/images/model/gomem/businesskim/body.png',
     links: [],
   },
   GwakChoonSik: {
     name: {
-      en: 'Gwak Chun Sik',
+      en: 'Gwak Choon Sik',
       ko: '곽춘식',
     },
-    description: '춘식.',
+    description: '일병 곽춘식. 대대장의 어항 관리가 보직이고 주로 던전앤파이터를 즐겨한다.',
     image: '/images/model/gomem/gwakchoonsik/body.png',
     links: [],
   },
-  GwonMin: {
+  KwonMin: {
     name: {
-      en: 'Gwon Min',
+      en: 'Kwon Min',
       ko: '권민',
     },
-    description: '권민.',
-    image: '/images/model/gomem/gwonmin/body.png',
+    description: '컨셉 같은 내추럴의 멤버. 대학교 입학 때 어머니가 사주신 옷을 입는다.',
+    image: '/images/model/gomem/Kwonmin/body.png',
     links: [],
   },
   DrDopamine: {
     name: {
-      en: 'DR. Dopamine',
+      en: 'Dr. Dopamine',
       ko: '도파민 박사',
     },
-    description: '도파민.',
+    description: '대한민국 최고의 박사라고 주장하는 박사? 메타버스 관련 주 매수 후 수익률 -11%를 찍었다고 한다.',
     image: '/images/model/gomem/drdophamine/body.png',
     links: [],
   },
@@ -174,7 +174,7 @@ export const GomemSeason2Members: Record<GomemMemberID, GomemMember> = {
       en: 'Fried Shrimp',
       ko: '새우튀김',
     },
-    description: '새우튀김.',
+    description: '도파민 박사 연구실에서 연구하는 대학원생. 새우튀김을 좋아한다.',
     image: '/images/model/gomem/friedshrimp/body.png',
     links: [],
   },
@@ -183,16 +183,16 @@ export const GomemSeason2Members: Record<GomemMemberID, GomemMember> = {
       en: 'Mitsune Haku',
       ko: '미츠네 하쿠',
     },
-    description: '하쿠.',
+    description: '마스터를 찾기 위해 왁타버스에 지원하게된 로봇. 기억을 잃었지만 새로운 가족을 왁타버스에서 찾았다.',
     image: '/images/model/gomem/mitsunehaku/body.png',
     links: [],
   },
   HaeruSeok: {
     name: {
-      en: 'HaeruSeok',
+      en: 'Haeru Seok',
       ko: '해루석',
     },
-    description: '루석.',
+    description: '왁타버스 내에서 미남 포지션을 맡고 있는 해루석.',
     image: '/images/model/gomem/haeruseok/body.png',
     links: [],
   },
@@ -201,7 +201,7 @@ export const GomemSeason2Members: Record<GomemMemberID, GomemMember> = {
       en: 'Dokgo HyeJi',
       ko: '독고혜지',
     },
-    description: '혜지.',
+    description: '느그고등학교에 다니고 있는 고등학생 혜지. 히키킹의 주장으로는 일진이라고 한다.',
     image: '/images/model/gomem/hyeji/body.png',
     links: [],
   },
@@ -210,7 +210,7 @@ export const GomemSeason2Members: Record<GomemMemberID, GomemMember> = {
       en: 'KimchiMandooBEONYeonTaekSASEUGA',
       ko: '김치만두번영택사스가',
     },
-    description: '김치만두.',
+    description: '느그 초등학교 5학년 김재민. 13남자. 우왁굳처럼 훌륭한 스트리퍼가 되는 것이 꿈이라고 한다.',
     image: '/images/model/gomem/kimchimandoo/body.png',
     links: [],
   },
@@ -219,16 +219,16 @@ export const GomemSeason2Members: Record<GomemMemberID, GomemMember> = {
       en: 'Secret Girl',
       ko: '비밀소녀',
     },
-    description: '비소.',
+    description: '"주님, 정의로운 도둑이 되도록 허락해주세요." 소녀지만 자주 이모로 불린다.',
     image: '/images/model/gomem/secretgirl/body.png',
     links: [],
   },
   DeokSuLee: {
     name: {
       en: 'Grandpa DeokSu Lee',
-      ko: '이덕수할아바이',
+      ko: '이덕수 할아바이',
     },
-    description: '하라바이.',
+    description: '1949년생 6월 23일 출생의 상남자 하라바이.',
     image: '/images/model/gomem/deoksulee/body.png',
     links: [],
   },
@@ -237,7 +237,7 @@ export const GomemSeason2Members: Record<GomemMemberID, GomemMember> = {
       en: 'Poong Sin',
       ko: '풍신',
     },
-    description: '풍신.',
+    description: '매직키드마수리가 26대 독자 청정 바람 마법사. 바람 마법을 쓴다.',
     image: '/images/model/gomem/poongsin/body.png',
     links: [],
   },
@@ -246,7 +246,7 @@ export const GomemSeason2Members: Record<GomemMemberID, GomemMember> = {
       en: 'Negative person',
       ko: '부정형 인간',
     },
-    description: '부정형.',
+    description: '매사에 부정적인 태도를 가진 멤버. 오디션에서 조차 부정적인 모습을 보여줬다.',
     image: '/images/model/gomem/negativeperson/body.png',
     links: [],
   },
@@ -255,7 +255,7 @@ export const GomemSeason2Members: Record<GomemMemberID, GomemMember> = {
       en: 'Hiki king',
       ko: '히키킹',
     },
-    description: '히키킹.',
+    description: '졸열잎 마을에서 온 우치하 일족의 히키킹.',
     image: '/images/model/gomem/hikiking/body.png',
     links: [],
   },
@@ -264,7 +264,7 @@ export const GomemSeason2Members: Record<GomemMemberID, GomemMember> = {
       en: 'Sophia',
       ko: '소피아',
     },
-    description: '소피아.',
+    description: '"선량한 시민 소피아라고 합니다." 직업 특성상 가면을 쓰고 있다.',
     image: '/images/model/gomem/sophia/body.png',
     links: [],
   },
@@ -273,7 +273,7 @@ export const GomemSeason2Members: Record<GomemMemberID, GomemMember> = {
       en: 'Roentgenium',
       ko: '뢴트게늄',
     },
-    description: '뢴트.',
+    description: '뇌절의 아이콘 뢴트게늄. 이세계 아이돌의 매니저를 맡고 있다.',
     image: '/images/model/gomem/roentgenium/body.png',
     links: [],
   },
@@ -282,7 +282,7 @@ export const GomemSeason2Members: Record<GomemMemberID, GomemMember> = {
       en: 'DogCat',
       ko: '개냥이'
     },
-    description: '개냥이',
+    description: '내추럴 컨셉의 개냥이 [데이터로만 남아 있고 등재 X]',
     image: '/images/model/gomem/dogcat/body.png',
     links: [],
   }
