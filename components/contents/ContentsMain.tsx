fc8b4dc57111fcfd05bcb5e27ef8a9d02310cab1
yrkim <yrkim@pocl.io>
2022-03-08 00:04:34 +0900

    메인화면 타이틀 추가

--- components/contents/ContentsMain.tsx
+++ components/contents/ContentsMain.tsx
@@ -6,6 +6,16 @@ export const ContentsMain: NextPage = () => {
   return (
     <div className={styles.main}>
       <div className={styles.background}></div>
+      <div className={styles.contents}>
+        <div className={styles.title}>
+          <h1>WakEntertainment만의 Contents</h1>
+          <h2>
+            <p>신선함</p>
+            <p>다양함</p>
+            <p>왁타버스의 매력에 빠져보세요!</p>
+          </h2>
+        </div>
+      </div>
     </div>
   );
 };
