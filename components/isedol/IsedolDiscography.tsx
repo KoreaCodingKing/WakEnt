a47672a19662d0d35e9208e27eeb113f29aadaa3
So-chiru <unleash9224@gmail.com>
2022-03-08 22:29:53 +0900

    Reorder discorgraphy groups

--- components/isedol/IsedolDiscography.tsx
+++ components/isedol/IsedolDiscography.tsx
@@ -1,6 +1,10 @@
 import { NextPage } from 'next';
 import { useRef, useState } from 'react';
-import { IsedolCover, IsedolOriginalSong, MemberCover } from '../../structs/cover';
+import {
+  IsedolCover,
+  IsedolOriginalSong,
+  MemberCover,
+} from '../../structs/cover';
 import { IsedolMemberID, IsedolMembers } from '../../structs/member';
 import styles from '../../styles/components/isedol/IsedolDiscography.module.scss';
 import Button from '../common/Button';
@@ -43,21 +47,7 @@ export const IsedolDiscography: NextPage = () => {
         </div>
       </section>
       <section className={styles.section}>
-        <h3 className={styles.sectionTitle}>Cover</h3>
-        <div className={styles.grid}>
-          {IsedolCover.map(cover => (
-            <YouTubeCard
-              key={`cover-${cover.id}`}
-              title={cover.title}
-              thumbnail={cover.thumbnail}
-              id={cover.id}
-              onClick={id => openYouTube(id)}
-            ></YouTubeCard>
-          ))}
-        </div>
-      </section>
-      <section className={styles.section}>
-        <h3 className={styles.sectionTitle}>Original Song</h3>
+        <h3 className={styles.sectionTitle}>Single</h3>
         <div className={styles.grid}>
           {IsedolOriginalSong.map(original => (
             <YouTubeCard
@@ -71,7 +61,21 @@ export const IsedolDiscography: NextPage = () => {
         </div>
       </section>
       <section className={styles.section}>
-        <h3 className={styles.sectionTitle}>개인 커버 송</h3>
+        <h3 className={styles.sectionTitle}>Group Cover</h3>
+        <div className={styles.grid}>
+          {IsedolCover.map(cover => (
+            <YouTubeCard
+              key={`cover-${cover.id}`}
+              title={cover.title}
+              thumbnail={cover.thumbnail}
+              id={cover.id}
+              onClick={id => openYouTube(id)}
+            ></YouTubeCard>
+          ))}
+        </div>
+      </section>
+      <section className={styles.section}>
+        <h3 className={styles.sectionTitle}>Member Cover</h3>
         <div className={styles.tabs} ref={tabRef}>
           {Object.keys(MemberCover).map((member: string, index: number) => (
             <div
@@ -80,22 +84,14 @@ export const IsedolDiscography: NextPage = () => {
                 element && membersTabCache.push(element)
               }
             >
-              <Button
-                onClick={() => {
-                  setTab(member);
-                }}
-                active={member === tab}
-              >
-                <p className={styles.sectionTitle}>{IsedolMembers[member as IsedolMemberID].name.ko}</p>
+              <Button onClick={() => setTab(member)} active={member === tab}>
+                {IsedolMembers[member as IsedolMemberID].name.ko}
               </Button>
             </div>
           ))}
         </div>
       </section>
       <section className={styles.section}>
-        <h3 className={styles.sectionTitle}>
-          {IsedolMembers[tab as IsedolMemberID].name.ko}
-        </h3>
         <div className={styles.grid}>
           {MemberCover[tab as IsedolMemberID].map(cover => (
             <YouTubeCard
