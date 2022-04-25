0b7022654c837557d91f0068fbaae02dc5446bb9
yrkim <yrkim@pocl.io>
2022-04-25 22:25:11 +0900

    사이드바 마진

--- components/contents/ContentsMain.tsx
+++ components/contents/ContentsMain.tsx
@@ -1,25 +1,34 @@
-import { NextPage } from 'next';
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { NextPage } from "next";
+import { useCallback, useEffect, useRef, useState } from "react";
 
-import { Content, ContentName, contentsList, Game } from "../../structs/contents";
+import {
+  Content,
+  ContentName,
+  contentsList,
+  Game,
+} from "../../structs/contents";
 
-import YouTubeCard from '../../components/isedol/YouTubeCard';
-import YouTubePlayerOverlay from '../../components/common/YouTubePlayerOverlay';
-import { useDebouncer, useIntersectionObserver } from '../../components/isedol/Members/Utils';
-import { classes } from '../../utils/class';
-import Sidebar from '../../components/contents/sidebar/Sidebar';
-import { MenuButton } from '../../components/isedol/IsedolHeader';
-import FilterListOverlay from '../../components/contents/overlay/filterListOverlay';
+import YouTubeCard from "../../components/isedol/YouTubeCard";
+import YouTubePlayerOverlay from "../../components/common/YouTubePlayerOverlay";
+import {
+  useDebouncer,
+  useIntersectionObserver,
+} from "../../components/isedol/Members/Utils";
+import { classes } from "../../utils/class";
+import Sidebar from "../../components/contents/sidebar/Sidebar";
+import { MenuButton } from "../../components/isedol/IsedolHeader";
+import FilterListOverlay from "../../components/contents/overlay/filterListOverlay";
 
-import styles from '../../styles/components/contents/ContentsMain.module.scss';
+import styles from "../../styles/components/contents/ContentsMain.module.scss";
 
 const ContentsMain: NextPage = () => {
   const [page, setPage] = useState<number>(1);
-  const [choosenGame, setChoosenGame] = useState<Game|string|null>('전체');
-  const [choosenContentName, setChoosenContentName] = useState<ContentName|null>();
+  const [choosenGame, setChoosenGame] = useState<Game | string | null>("전체");
+  const [choosenContentName, setChoosenContentName] =
+    useState<ContentName | null>();
   const [contents, setContents] = useState<Content[]>(contentsList);
   const [currentContents, setCurrentContents] = useState<Content[]>([]);
-  const [youtubeID, setYoutubeID] = useState<string>('');
+  const [youtubeID, setYoutubeID] = useState<string>("");
   const [openPlayer, setOpenPlayer] = useState<boolean>(false);
   const [showSidebar, setShowSidebar] = useState<boolean>();
   const [openDetail, setOpenDetail] = useState<boolean>(false);
@@ -45,38 +54,50 @@ const ContentsMain: NextPage = () => {
     contentsPage,
     useCallback(() => {
       setTimeout(() => {
-        setPage(page => page+1);
+        setPage((page) => page + 1);
         setCurrentContents(
           contents.slice(
             0,
-            (contents.length >= (page+2)*numberOfContentsByPage) ? (page+2)*numberOfContentsByPage : contents.length
+            contents.length >= (page + 2) * numberOfContentsByPage
+              ? (page + 2) * numberOfContentsByPage
+              : contents.length
           )
         );
       }, 200);
     }, [page, contents])
   );
 
-  const filterContents = useCallback((game: Game|string, contentName?: ContentName) => {
-    if (!container.current) {
-      return;
-    }
-    setPage(1);
-    container.current.scrollTo(0, 0);
-    setChoosenGame(game);
-    setOpenDetail(false);
-    if (game === '전체') {
-      setChoosenContentName(null);
-      setContents(contentsList);
-      return;
-    }
-    if (game && !contentName) {
-      setChoosenContentName(null);
-      setContents(contentsList.filter((content: Content) => content.game === game));
-      return;
-    }
-    setChoosenContentName(contentName);
-    setContents(contentsList.filter((content: Content) => content.game === game && content.content === contentName));
-  }, []);
+  const filterContents = useCallback(
+    (game: Game | string, contentName?: ContentName) => {
+      if (!container.current) {
+        return;
+      }
+      setPage(1);
+      container.current.scrollTo(0, 0);
+      setChoosenGame(game);
+      setOpenDetail(false);
+      if (game === "전체") {
+        setChoosenContentName(null);
+        setContents(contentsList);
+        return;
+      }
+      if (game && !contentName) {
+        setChoosenContentName(null);
+        setContents(
+          contentsList.filter((content: Content) => content.game === game)
+        );
+        return;
+      }
+      setChoosenContentName(contentName);
+      setContents(
+        contentsList.filter(
+          (content: Content) =>
+            content.game === game && content.content === contentName
+        )
+      );
+    },
+    []
+  );
 
   useEffect(() => {
     setCurrentContents(contents.slice(0, numberOfContentsByPage));
@@ -108,45 +129,45 @@ const ContentsMain: NextPage = () => {
         close={() => setOpenPlayer(false)}
       ></YouTubePlayerOverlay>
       <div className={styles.inner_container}>
-        {showSidebar && (
-          <Sidebar selectContents={filterContents}></Sidebar>
-        )}
+        {showSidebar && <Sidebar selectContents={filterContents}></Sidebar>}
         {!showSidebar && (
           <div className={styles.filter_box}>
             <MenuButton open={openDetail} onClick={run}></MenuButton>
           </div>
         )}
-        <div className={styles.contents_wrapper}
-          ref={container}>
-          <FilterListOverlay open={openDetail}
+        <div className={styles.contents_wrapper} ref={container}>
+          <FilterListOverlay
+            open={openDetail}
             selectContents={filterContents}
           ></FilterListOverlay>
           <div className={styles.filter_boxes}>
-            {choosenGame && choosenGame !== '전체' && (
+            {choosenGame && choosenGame !== "전체" && (
               <div className={styles.filter_game}>
                 <p>{choosenGame}</p>
-                <svg xmlns="http://www.w3.org/2000/svg"
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
                   fill="#fff"
                   viewBox="0 0 32 32"
                   width="16px"
                   height="16px"
-                  onClick={() => filterContents('전체')}>
-                  <path
-                    d="M 7.21875 5.78125 L 5.78125 7.21875 L 14.5625 16 L 5.78125 24.78125 L 7.21875 26.21875 L 16 17.4375 L 24.78125 26.21875 L 26.21875 24.78125 L 17.4375 16 L 26.21875 7.21875 L 24.78125 5.78125 L 16 14.5625 Z"/>
+                  onClick={() => filterContents("전체")}
+                >
+                  <path d="M 7.21875 5.78125 L 5.78125 7.21875 L 14.5625 16 L 5.78125 24.78125 L 7.21875 26.21875 L 16 17.4375 L 24.78125 26.21875 L 26.21875 24.78125 L 17.4375 16 L 26.21875 7.21875 L 24.78125 5.78125 L 16 14.5625 Z" />
                 </svg>
               </div>
             )}
             {choosenContentName && (
               <div className={styles.filter_contents}>
                 <p>{choosenContentName}</p>
-                <svg xmlns="http://www.w3.org/2000/svg"
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
                   fill="#fff"
                   viewBox="0 0 32 32"
                   width="16px"
                   height="16px"
-                  onClick={() => filterContents(choosenGame!)}>
-                  <path
-                    d="M 7.21875 5.78125 L 5.78125 7.21875 L 14.5625 16 L 5.78125 24.78125 L 7.21875 26.21875 L 16 17.4375 L 24.78125 26.21875 L 26.21875 24.78125 L 17.4375 16 L 26.21875 7.21875 L 24.78125 5.78125 L 16 14.5625 Z"/>
+                  onClick={() => filterContents(choosenGame!)}
+                >
+                  <path d="M 7.21875 5.78125 L 5.78125 7.21875 L 14.5625 16 L 5.78125 24.78125 L 7.21875 26.21875 L 16 17.4375 L 24.78125 26.21875 L 26.21875 24.78125 L 17.4375 16 L 26.21875 7.21875 L 24.78125 5.78125 L 16 14.5625 Z" />
                 </svg>
               </div>
             )}
@@ -166,15 +187,17 @@ const ContentsMain: NextPage = () => {
           </div>
           {page < Math.ceil(contents.length / 24) && (
             <div className={classes(styles.contents, styles.emptyCards)}>
-              {Array(6).fill(0).map((_, index) => {
-                return (
-                  <YouTubeCard
-                    key={`content-${index}`}
-                    title=''
-                    id=''
-                  ></YouTubeCard>
-                );
-              })}
+              {Array(6)
+                .fill(0)
+                .map((_, index) => {
+                  return (
+                    <YouTubeCard
+                      key={`content-${index}`}
+                      title=""
+                      id=""
+                    ></YouTubeCard>
+                  );
+                })}
             </div>
           )}
         </div>
