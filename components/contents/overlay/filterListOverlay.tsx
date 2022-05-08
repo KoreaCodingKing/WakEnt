dac6365965c380d922496f3c6100a4945998b9f3
yrkim <yrkim@pocl.io>
2022-05-08 15:36:54 +0900

    사이드바 수정

--- components/contents/overlay/filterListOverlay.tsx
+++ components/contents/overlay/filterListOverlay.tsx
@@ -1,85 +1,87 @@
-import Image from 'next/image';
-import { ContentByGame, ContentName, Game } from '../../../structs/contents';
-import { classes } from '../../../utils/class';
-import styles from '../../../styles/components/contents/overlay/filterListOverlay.module.scss';
-import { motion, Variants } from 'framer-motion';
+import Image from "next/image";
+import { ContentByGame, ContentName, Game } from "../../../structs/contents";
+import { classes } from "../../../utils/class";
+import styles from "../../../styles/components/contents/overlay/filterListOverlay.module.scss";
+import { motion, Variants } from "framer-motion";
 
 interface FilterListOverlay {
-  open: boolean,
-  selectContents: (game: Game|string, contentName?: ContentName) => void,
+  open: boolean;
+  selectContents: (game: Game | string, contentName?: ContentName) => void;
 }
 
-const FilterListOverlay = ({open, selectContents}: FilterListOverlay) => {
+const FilterListOverlay = ({ open, selectContents }: FilterListOverlay) => {
   const variants: Variants = {
     hover: () => ({
-      color: '#1667e0',
-      fontSize: '32px',
-      fontWeight: '500',
+      color: "#1667e0",
+      fontSize: "32px",
+      fontWeight: "500",
       transition: {
-        type: 'spring',
-        duration: 0.5
-      }
-    })
+        type: "spring",
+        duration: 0.5,
+      },
+    }),
   };
 
   return (
     <div className={classes(styles.overlay, open && styles.open)}>
       <div className={styles.background}></div>
       <div className={styles.contents}>
-        <div className={styles.game}
-          onClick={() => selectContents('전체')}>
+        <div className={styles.game} onClick={() => selectContents("전체")}>
           <div className={styles.icon_wrapper}>
-            <div className={styles.game_icon}>
-              <div className={styles.totalgame}>
-              </div>
-            </div>
             <p>전체 보기</p>
           </div>
         </div>
         {(Object.keys(ContentByGame) as Game[]).map((game, index) => {
           return (
-            <div key={`game-${index}`}
-              className={styles.game}>
-              <div className={styles.icon_wrapper}
-                onClick={() => selectContents(game)}>
-                <div className={
-                  classes(
+            <div key={`game-${index}`} className={styles.game}>
+              <div
+                className={styles.icon_wrapper}
+                onClick={() => selectContents(game)}
+              >
+                <div
+                  className={classes(
                     styles.game_icon,
-                    game === 'WakSiries' && styles.wakLogo
-                  )
-                }>
+                    game === "WakSiries" && styles.wakLogo
+                  )}
+                >
                   {ContentByGame[game].image && (
                     <Image
                       className={styles.image}
-                      src={ContentByGame[game].image || ''}
+                      src={ContentByGame[game].image || ""}
                       blurDataURL={ContentByGame[game].image}
-                      placeholder='blur'
-                      layout='fill'
+                      placeholder="blur"
+                      layout="fill"
                       priority
                       alt="게임 아이콘"
                     ></Image>
                   )}
                   {!ContentByGame[game].image && (
-                    <div className={styles.emptyImage}>
-                    </div>
+                    <div className={styles.emptyImage}></div>
                   )}
                 </div>
                 <p>{ContentByGame[game].name}</p>
               </div>
-              <div
-                className={styles.dropMenu}>
+              <div className={styles.dropMenu}>
                 {ContentByGame[game].contentName.map((conentName, index) => {
-                  return (<motion.p
-                    key={`contentName-${index}`}
-                    className={styles.contentsName}
-                    whileHover='hover'
-                    variants={variants}
-                    style={{
-                      '--index': `${(index + 1) / (ContentByGame[game].contentName.length + 1)}`
-                    } as React.CSSProperties}
-                    onClick={() => selectContents(game, conentName)}>
-                    {conentName}
-                  </motion.p>);
+                  return (
+                    <motion.p
+                      key={`contentName-${index}`}
+                      className={styles.contentsName}
+                      whileHover="hover"
+                      variants={variants}
+                      style={
+                        {
+                          "--index": `${
+                            (index + 1) /
+                            (ContentByGame[game].contentName.length + 1)
+                          }`,
+                        } as React.CSSProperties
+                      }
+                      onClick={() => selectContents(game, conentName)}
+                    >
+                      {conentName}
+                    </motion.p>
+                  );
                 })}
               </div>
             </div>
--- components/contents/sidebar/Sidebar.tsx
+++ components/contents/sidebar/Sidebar.tsx
@@ -1,30 +1,30 @@
-import React, { useCallback, useState } from 'react';
-import Image from 'next/image';
-import { ContentByGame, Game, ContentName } from '../../../structs/contents';
-import styles from '../../../styles/components/contents/sidebar/Sidebar.module.scss';
-import { useDebouncer } from '../../isedol/Members/Utils';
-import { useDeviceWidthLimiter } from '../../../utils/device';
-import { motion, Variants } from 'framer-motion';
-import { classes } from '../../../utils/class';
+import React, { useCallback, useState } from "react";
+import Image from "next/image";
+import { ContentByGame, Game, ContentName } from "../../../structs/contents";
+import styles from "../../../styles/components/contents/sidebar/Sidebar.module.scss";
+import { useDebouncer } from "../../isedol/Members/Utils";
+import { useDeviceWidthLimiter } from "../../../utils/device";
+import { motion, Variants } from "framer-motion";
+import { classes } from "../../../utils/class";
 
 interface SidebarProps {
-  selectContents: (game: Game|string, ContentName?: ContentName) => void,
+  selectContents: (game: Game | string, ContentName?: ContentName) => void;
 }
 
-const Sidebar = ({selectContents}: SidebarProps) => {
-  const [hoverGame, setHoverGame] = useState<Game|null>();
+const Sidebar = ({ selectContents }: SidebarProps) => {
+  const [hoverGame, setHoverGame] = useState<Game | null>();
   const isMobile = useDeviceWidthLimiter(1124);
 
   const variants: Variants = {
     hover: () => ({
-      color: '#1667e0',
-      fontSize: '22px',
-      fontWeight: '500',
+      color: "#1667e0",
+      fontSize: "22px",
+      fontWeight: "500",
       transition: {
-        type: 'spring',
-        duration: 0.5
-      }
-    })
+        type: "spring",
+        duration: 0.5,
+      },
+    }),
   };
 
   const [run, cancel] = useDebouncer((id: Game | null) => {
@@ -47,60 +47,63 @@ const Sidebar = ({selectContents}: SidebarProps) => {
 
   return (
     <div className={styles.select_box}>
-      <div className={styles.game}
-        onClick={() => selectContents('전체')}>
-        <div className={styles.totalgame_icon}>
-          <div className={styles.totalgame}>
-          </div>
-        </div>
+      <div className={styles.game} onClick={() => selectContents("전체")}>
         <p>전체 보기</p>
       </div>
       {(Object.keys(ContentByGame) as Game[]).map((game, index) => {
         return (
-          <div key={`game-${index}`}
+          <div
+            key={`game-${index}`}
             className={styles.game}
             onMouseEnter={() => onGameIconMouseEnter(game)}
-            onMouseOut={() => onGameIconMouseOut()}>
-            <div className={styles.game_wrapper}
-              onClick={() => selectContents(game)}>
-              <div className={
-                classes(
+            onMouseOut={() => onGameIconMouseOut()}
+          >
+            <div
+              className={styles.game_wrapper}
+              onClick={() => selectContents(game)}
+            >
+              <div
+                className={classes(
                   styles.game_icon,
-                  game === 'WakSiries' && styles.wakLogo
-                )
-              }>
+                  game === "WakSiries" && styles.wakLogo
+                )}
+              >
                 {ContentByGame[game].image && (
                   <Image
                     className={styles.image}
-                    src={ContentByGame[game].image || ''}
+                    src={ContentByGame[game].image || ""}
                     blurDataURL={ContentByGame[game].image}
-                    placeholder='blur'
-                    layout='fill'
+                    placeholder="blur"
+                    layout="fill"
                     priority
                     alt="게임 아이콘"
                   ></Image>
                 )}
                 {!ContentByGame[game].image && (
-                  <div className={styles.emptyImage}>
-                  </div>
+                  <div className={styles.emptyImage}></div>
                 )}
               </div>
               <p>{ContentByGame[game].name}</p>
             </div>
             {hoverGame && (
-              <div
-                className={styles.dropMenu}>
+              <div className={styles.dropMenu}>
                 {ContentByGame[game].contentName.map((conentName, index) => {
                   return (
                     <motion.p
                       key={`contentName-${index}`}
                       className={styles.contentsName}
-                      whileHover='hover'
+                      whileHover="hover"
                       variants={variants}
-                      style={{
-                        '--index': `${(index + 1) / (ContentByGame[game].contentName.length + 1)}`
-                      } as React.CSSProperties}
-                      onClick={() => selectContents(game, conentName)}>
+                      style={
+                        {
+                          "--index": `${
+                            (index + 1) /
+                            (ContentByGame[game].contentName.length + 1)
+                          }`,
+                        } as React.CSSProperties
+                      }
+                      onClick={() => selectContents(game, conentName)}
+                    >
                       {conentName}
                     </motion.p>
                   );
