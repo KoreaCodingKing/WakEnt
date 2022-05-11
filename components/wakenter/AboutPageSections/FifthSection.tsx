128a488bbc23e20c8d854274565b9c54da6d0890
yrkim <yrkim@pocl.io>
2022-05-12 00:31:37 +0900

    프로미넌스 진 기념 대표 임시 교체

--- components/wakenter/AboutPageSections/FifthSection.tsx
+++ components/wakenter/AboutPageSections/FifthSection.tsx
@@ -7,9 +7,11 @@ import Typer from "../../common/Typer";
 import Link from "next/link";
 import LinkToIcon from "../../common/icons/LinkTo";
 
-const FifthSection = ({className, onScroll}: AboutSectionProps) => {
+const FifthSection = ({ className, onScroll }: AboutSectionProps) => {
   const [scrollProgress, setScrollProgress] = useState<number>(0);
-  const link = 'https://www.youtube.com/user/woowakgood/search?query=%EB%85%B8%EA%B0%80%EB%A6%AC';
+  const link =
+    "https://www.youtube.com/playlist?list=PLZNwpHxpI4EhwAuQfL3b_Urc-XsYRzB_3";
+  // "https://www.youtube.com/user/woowakgood/search?query=%EB%85%B8%EA%B0%80%EB%A6%AC";
 
   useEffect(() => {
     onScroll(4, (top, height) => {
@@ -22,23 +24,34 @@ const FifthSection = ({className, onScroll}: AboutSectionProps) => {
       <div className={styles.section_container}>
         <div className={styles.contents_wrapper}>
           <div className={styles.image_wrapper}>
-            <Image src={'/images/member/wakgood.png'} alt="우왁굳 대표 정장사진" layout="fill"></Image>
+            {/* <Image src={'/images/member/wakgood.png'} alt="우왁굳 대표 정장사진" layout="fill"></Image> */}
+            <Image
+              src={"/images/member/front/gosegu.png"}
+              alt="우왁굳 대표 정장사진"
+              layout="fill"
+            ></Image>
           </div>
           <div className={styles.motto_container}>
             <p className={styles.motto_top_quote}>&quot;</p>
-            <Typer progress={scrollProgress} className={styles.motto}>사실 난 메타버스가 뭔지 잘 모른다.</Typer>
+            <Typer progress={scrollProgress} className={styles.motto}>
+              사실 난 메타버스가 뭔지 잘 모른다.
+            </Typer>
             <p className={styles.motto_bottom_quote}>&quot;</p>
-            <p className={styles.motto_desc}>- 왁엔터테인먼트 대표 우왁굳, 왁타버스에 대한 자신감을 내보이며</p>
+            {/* <p className={styles.motto_desc}>- 왁엔터테인먼트 대표 우왁굳, 왁타버스에 대한 자신감을 내보이며</p> */}
+            <p className={styles.motto_desc}>
+              - 왁엔터테인먼트 대표 고세구, 왁타버스에 대한 자신감을 내보이며
+            </p>
             <div
               className={styles.link}
               tabIndex={100}
-              onKeyDown={ev =>
-                ev.key === 'Enter' && (window.location.href = link)
+              onKeyDown={(ev) =>
+                ev.key === "Enter" && (window.location.href = link)
               }
             >
               <Link href={link} passHref>
                 <a>
-                  대표의 삶에 대한 철학과 인생 엳듣기 <LinkToIcon width={16}></LinkToIcon>
+                  대표의 삶에 대한 철학과 인생 엳듣기{" "}
+                  <LinkToIcon width={16}></LinkToIcon>
                 </a>
               </Link>
             </div>
--- components/wakenter/AboutPageSections/FourthSection.tsx
+++ components/wakenter/AboutPageSections/FourthSection.tsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { motion, useSpring } from "framer-motion";
 import { AboutSectionProps } from "../../../pages/about";
 import { classes } from "../../../utils/class";
-import styles from '../../../styles/components/wakenter/AboutPageSections/FourthSection.module.scss';
+import styles from "../../../styles/components/wakenter/AboutPageSections/FourthSection.module.scss";
 import Image from "next/image";
 import { clamp } from "../../../utils/number";
 
@@ -26,28 +26,46 @@ const FourthSection = ({ className, onScroll }: AboutSectionProps) => {
     <section className={classes(className)} data-index={3}>
       <div className={styles.section_container}>
         <div className={styles.contents_wrapper}>
-          <motion.div className={styles.infra_introduce}
+          <motion.div
+            className={styles.infra_introduce}
             style={{
-              opacity: firstOpacity
-            }}>
+              opacity: firstOpacity,
+            }}
+          >
             <div className={styles.image_wrapper}>
-              <Image src={'/images/building/recording_room_mixer.png'} alt="믹서 기기" layout="fill"></Image>
+              <Image
+                src={"/images/building/recording_room_mixer.png"}
+                alt="믹서 기기"
+                layout="fill"
+              ></Image>
             </div>
             <div className={styles.title_wrapper}>
               <p className={styles.title}>업계 최고 오디오 장비 도입</p>
-              <p className={styles.subtitle}>회사 스튜디오 내에 업계 최고급 오디오 장비를 구비하여 소비자에게 최고의 경험을 선사하고자 노력하고 있습니다.</p>
+              <p className={styles.subtitle}>
+                회사 스튜디오 내에 업계 최고급 오디오 장비를 구비하여 소비자에게
+                최고의 경험을 선사하고자 노력하고 있습니다.
+              </p>
             </div>
           </motion.div>
-          <motion.div className={styles.crew_container}
+          <motion.div
+            className={styles.crew_container}
             style={{
-              opacity: secondOpacity
-            }}>
+              opacity: secondOpacity,
+            }}
+          >
             <div className={styles.image_wrapper}>
-              <Image src={'/images/member/benzenpro.jpg'} alt="벤젠프로" layout="fill"></Image>
+              <Image
+                src={"/images/member/benzenpro.jpg"}
+                alt="벤젠프로"
+                layout="fill"
+              ></Image>
             </div>
             <div className={styles.title_wrapper}>
               <p className={styles.title}>세계 최고의 스태프</p>
-              <p className={styles.subtitle}>각 분야 극한의 알잘딱 실력자들이 모인 스튜디오에서 원하는 것을 모두 이룰 수 있습니다.</p>
+              <p className={styles.subtitle}>
+                각 분야 극한의 알잘딱 실력자들이 모인 스튜디오에서 원하는 것을
+                모두 이룰 수 있습니다.
+              </p>
             </div>
           </motion.div>
         </div>
